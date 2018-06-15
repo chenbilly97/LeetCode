998177700f3094d44e9db8981a70f67ff7eb96ce
zijianchen <chenbilly97@gmail.com>
2018-06-15 10:55:08 -0400

    Two Sum III - Data structure design solution

---
+++ 170_Two_Sum_III.py
@@ -0,0 +1,34 @@
+class TwoSum:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.__nums = {}
+
+    def add(self, number):
+        """
+        Add the number to an internal data structure..
+        :type number: int
+        :rtype: void
+        """
+        if number in self.__nums:
+            self.__nums[number] += 1
+        else:
+            self.__nums[number] = 1
+
+    def find(self, value):
+        """
+        Find if there exists any pair of numbers which sum is equal to the value.
+        :type value: int
+        :rtype: bool
+        """
+        for val in self.__nums:
+            if (value - val in self.__nums and (((value - val) != val) or self.__nums[val] > 1)):
+                return True
+        return False;
+
+# Your TwoSum object will be instantiated and called as such:
+# obj = TwoSum()
+# obj.add(number)
+# param_2 = obj.find(value)
