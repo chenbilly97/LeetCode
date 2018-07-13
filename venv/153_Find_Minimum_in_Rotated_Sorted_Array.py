dcae08d4075c5402dbf4038ec24ca7a66077ecd8
zijianchen <chenbilly97@gmail.com>
2018-07-13 12:28:38 -0400

    153. Find Minimum in Rotated Sorted Array Solution

---
+++ venv/153_Find_Minimum_in_Rotated_Sorted_Array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        i = 0
+        j = len(nums) - 1
+        while i < j:
+            m = i + (int)((j - i) / 2);
+            if nums[m] > nums[j]:
+                i = m + 1
+            else:
+                j = m
+        return nums[i]
+
+
+
+
+
+
